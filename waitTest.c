1ec271848891650f1c0783e4c3330dc974ae0402
AntonDanylenko <adanylenko@stuy.edu>
2018-11-16 11:02:48 -0500

    s

---
+++ waitTest.c
@@ -0,0 +1,27 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+  printf("Intitial message\n");
+  int childA = fork();
+  printf("ChildA Pid: %d\n", childA);
+  int x = 0;
+  int status;
+  while(x<8){
+    waitpid(&childA, status, 0);
+    sleep(1);
+  }
+  printf("ChildA done waiting\n");
+  int childB = fork();
+  printf("ChildB Pid: %d\n", childB);
+  x = 0;
+  int status;
+  while (x<14){
+    waitpid(&childB, status, 0);
+    sleep(1);
+  }
+  printf("ChildB done waiting\n");
+}

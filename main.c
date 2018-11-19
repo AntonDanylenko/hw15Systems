7cf1004eeaeee41189c2ac6e6d1c86fad2ab4306
Anton Danylenko <adanylenko@stuy.edu>
2018-11-18 22:43:12 -0500

    s

--- main.c
+++ main.c
@@ -2,19 +2,22 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
 
 int main(){
-  printf("Intitial message\n");
+  srand(time(NULL));
+  int shleep = rand();
+  printf("Initial. Parent pid: %d\n", getpid());
   int childA = fork();
-  printf("ChildA Pid: %d\n", childA);
-  int x = 0;
-  int status;
-  while(x<8){
-    waitpid(&childA, status, 0);
-    sleep(1);
+  if(!childA){
+    printf("ChildA Pid: %d\n", getpid());
+    int x = 0;
+    while(x<shleep){
+      wait(&childA);
+      sleep(1);
+    }
+    printf("ChildA done waiting\n");
   }
-  printf("ChildA done waiting\n");
   /*int childB = fork();
   printf("ChildB Pid: %d\n", childB);
   x = 0;
